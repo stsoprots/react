b202838865dc4830301fa5072ce7a5dbfeef559b
stsoprots <stso.prots@gmail.com>
2026-08-22 17:16:18 +0300

    forms uncontrolled

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,10 @@
+export const FormComponent = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+            </form>
+        </div>
+    );
+};
