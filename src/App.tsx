79c179f6967ddb5f25440cd88a90150847d89c1d
stsoprots <stso.prots@gmail.com>
2026-08-24 17:28:05 +0300

    useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import {FormComponent} from "./components/FormComponent.tsx";
 
 function App() {
 
     return (
         <>
-
+            <FormComponent/>
         </>
     )
 }
---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,27 @@
+import {useForm} from "react-hook-form";
+
+interface IFormProps {
+    username: string,
+    password: string,
+    age: number
+}
+
+export const FormComponent = () => {
+
+    const {handleSubmit, register} = useForm<IFormProps>();
+
+    const customHandler = (formDataProps: IFormProps) => {
+        console.log(formDataProps);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(customHandler)}>
+                <input type="text" {...register('username')}/>
+                <input type="text" {...register('password')}/>
+                <input type="number" {...register('age')}/>
+                <button>send</button>
+            </form>
+        </div>
+    );
+};
